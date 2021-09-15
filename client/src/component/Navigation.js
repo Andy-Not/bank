f0a3d057086c488cdfc88751d37200fe56613d2b
joandy <joandyalejo1292000@gmail.com>
2021-09-15 02:27:18 -0400

    navigation bar completed

--- client/src/component/Navigation.js
+++ client/src/component/Navigation.js
@@ -1,8 +1,16 @@
+import classes from './navigation.module.css';
+import logo from '../assets/images/logo.png'
 const Navigation = () => {
   return(
-      <div>
-          hkdhasdjashdk
-      </div>
+      <nav className={classes['nav-wrapper']}>
+          <img src={logo}/>
+          <ul className={classes['menu']}>
+              <li>PERSONAL <i className={classes['down-arrow']}/> </li>
+              <li>BUSINESS <i className={classes['down-arrow']}/> </li>
+              <li>RESOURCES <i className={classes['down-arrow']}/> </li>
+              <li>WHY US? <i className={classes['down-arrow']}/> </li>
+          </ul>
+      </nav>
   )
 }
 export default Navigation;
