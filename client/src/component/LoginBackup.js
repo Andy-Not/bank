bf15979f47794626ec2f3c7ceffa6bdb5edb6111
joandy alejo garcia <63996685+Andy-Not@users.noreply.github.com>
2021-09-22 22:18:19 -0400

    added a password input field, error message, and also i added a Mui theme override to the app.js component.

--- client/src/component/LoginBackup.js
+++ client/src/component/LoginBackup.js
@@ -3,14 +3,27 @@ import logo from '../assets/images/loginlogo2.png'
 import Card from '@mui/material/Card';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+
+
 const LoginBackup = () => {
+    const btnStyles = {
+        padding:'0.8rem',
+        borderRadius:'9px',
+        fontWeight: '600',
+
+    }
+
     return(
         <div className={classes.background}>
             <form action="">
                 <Card style={{borderRadius: '10px'}} variant="outlined" className={classes.wrapper}>
                     <img src={logo} alt="log-in form top logo"/>
                     <div className={classes['input-section']}>
-                        <TextField id="outlined-basic" label="Username" variant="outlined" />
+                        <div className={classes['input-error']}>
+                            not a valid username
+                        </div>
+                        <TextField classes={classes['text-input']} color={'primary'} id="outlined-basic" label="Username" type={'text'} variant="outlined" sx={{mb: 3}}/>
+                        <TextField classes={classes['text-input']} color={'primary'} id="outlined-basic" label="Password" type={'password'} variant="outlined" />
                         <div className={classes.forgot}>
                             Forgot?
                         </div>
@@ -19,7 +32,7 @@ const LoginBackup = () => {
                         <div>
                             First Time User? Enroll now.
                         </div>
-                        <Button style={{backgroundColor:'rgb(182, 2, 80)', padding:'0.8rem', borderRadius:'9px'}} type={"submit"} variant="contained">Continue</Button>
+                        <Button color={"primary"} style={btnStyles} type={"submit"} variant="contained">Continue</Button>
                     </div>
                 </Card>
             </form>
